b781ac2b2d32eb246eea0937ff166d07974bc0c6
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2026-04-28 15:31:49 -0300

    feat(env): add optional SEQ_URL and SEQ_API_KEY fields

--- src/env.ts
+++ src/env.ts
@@ -14,7 +14,9 @@ export const env = createEnv({
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     GOOGLE_REDIRECT_URL: z.string().min(1),
-    FRONTEND_AUTH_REDIRECT_URL: z.string().min(1)
+    FRONTEND_AUTH_REDIRECT_URL: z.string().min(1),
+    SEQ_URL: z.string().url().optional(),
+    SEQ_API_KEY: z.string().optional(),
   },
   runtimeEnv: process.env
 });
